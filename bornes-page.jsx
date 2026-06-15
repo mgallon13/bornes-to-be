bf92bb525ce3079fce9760124acdd408994a6618
Mael <maael.gaallon@gmail.com>
2026-06-15 14:16:22 +0200

    Auto-save: 2026-06-15 14:16

--- bornes-page.jsx
+++ bornes-page.jsx
@@ -313,7 +313,7 @@ const RESULT_TIERS = {
       {
         label: 'Recommandé',
         brand: 'Hager', model: 'witty one 7,4 kW TIC',
-        visualType: 'murale', iconBg: '#ecf2f8', photo: null,
+        visualType: 'murale', iconBg: '#ecf2f8', photo: 'img/bornes/hager-witty-one.jpg',
         power: '7,4 kW monophasé', charge: '100 km en ~3 h',
         price: 'À partir de 985 € TTC',
         pros: [
